e3688ef2552f47ce94593433ca9c07773d29badf
ark120202 <ark120202@gmail.com>
2020-04-19 18:50:22 +0500

    fix: leaderboard calculation is not cached

--- Server/Services/RatingService.cs
+++ Server/Services/RatingService.cs
@@ -2,31 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Hosting;
 using Server.Models;
 
 namespace Server.Services
 {
     public class RatingService
     {
+        private const string CacheKey = "Leaderboards";
         private readonly AppDbContext _context;
-        private const int NumberOfTopPlayers = 100;
-        private const int MaximumRatingChange = 55;
-        private const int BaseRatingChange = 30;
+        private readonly IWebHostEnvironment _environment;
+        private readonly IMemoryCache _cache;
 
-        public RatingService(AppDbContext context)
+        public RatingService(AppDbContext context, IWebHostEnvironment environment, IMemoryCache cache)
         {
             _context = context;
+            _environment = environment;
+            _cache = cache;
         }
 
-        public async Task<List<LeaderboardPlayer>> GetLeaderboard()
-        {
-            return await _context.Players
-                            .OrderByDescending(p => p.Rating12v12)
-                            .Take(NumberOfTopPlayers)
-                            .Select(p => new LeaderboardPlayer { SteamId = p.SteamId.ToString(), Rating = p.Rating12v12 })
-                            .ToListAsync();
-        }
+        public async Task<List<LeaderboardPlayer>> GetLeaderboard() =>
+            await _cache.GetOrCreateAsync(CacheKey, async entry =>
+            {
+                entry.AbsoluteExpirationRelativeToNow = _environment.IsProduction() ? TimeSpan.FromMinutes(5) : TimeSpan.FromTicks(1);
+                return await _context.Players
+                    .OrderByDescending(p => p.Rating12v12)
+                    .Take(100)
+                    .Select(p => new LeaderboardPlayer { SteamId = p.SteamId.ToString(), Rating = p.Rating12v12 })
+                    .ToListAsync();
+            });
 
         public Dictionary<ulong, PlayerRatingChange> RecordRankedMatch(IEnumerable<MatchPlayer> matchPlayers, ushort winnerTeam)
         {
@@ -40,9 +47,11 @@ namespace Server.Services
             return GetPlayersChange(teams, scoreDelta);
         }
 
+        private const int BaseRatingChange = 30;
+        private const int MaximumRatingChange = 55;
         private int CalculateScoreDelta(double averageWinningRating, double averageLosingRating)
         {
-            // e.g. -(1900 - 2100) / 40 = 5. Meaning winning team will get 5 more points 
+            // e.g. -(1900 - 2100) / 40 = 5. Meaning winning team will get 5 more points
             // than the base as their average was weaker
             var scoreDeltaDouble = -(averageWinningRating - averageLosingRating) / 40;
             var scoreDelta = (int)(Math.Round(scoreDeltaDouble, 0, MidpointRounding.AwayFromZero));
